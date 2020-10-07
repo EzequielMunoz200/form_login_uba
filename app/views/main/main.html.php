596ec850faf94c46576982e4021ae0958bc05a77
ezequiel <eemunoz.e@gmail.com>
2020-10-07 17:38:54 +0200

    Added js

--- app/views/main/main.html.php
+++ app/views/main/main.html.php
@@ -4,47 +4,47 @@
         <h2>C'est gratuit (et ça le restera toujours)</h2>
     </div>
 
-    <form id="form-register" action="<?= $router->generate('register') ?>" method="POST">
+    <form id="form-register" action="<?= $router->generate('register') ?>" method="POST" novalidate='novalidate'>
 
         <div class="row">
 
             <div class="form-group col-6">
-                <input class="form-control" type="text" name="firstname" id="firstname" placeholder="Prénom">
+                <input class="form-control" data-valide type="text" name="firstname" id="firstname" placeholder="Prénom">
             </div>
 
 
             <div class="form-group col-6">
-                <input class="form-control" type="text" name="lastname" id="lastname" placeholder="Nom de famille">
+                <input class="form-control" data-valide type="text" name="lastname" id="lastname" placeholder="Nom de famille">
             </div>
 
             <div class="form-group col-12">
                 <!--label for="email"></label-->
-                <input class="form-control" type="email" name="email" id="email" placeholder="Numéro de mobile ou email">
+                <input class="form-control" data-valide type="email" name="email" id="email" placeholder="Numéro de mobile ou email">
             </div>
 
             <div class="form-group col-12">
                 <!--label for="email"></label-->
-                <input class="form-control" type="email" name="email-confirmation" id="email-confirmation" placeholder="Confirmer numéro de mobile ou email">
+                <input class="form-control" data-valide type="email" name="email-confirmation" id="email-confirmation" placeholder="Confirmer numéro de mobile ou email">
             </div>
 
             <div class="form-group col-12">
                 <!--label for="password"></label-->
-                <input class="form-control" type="password" name="password" id="password" placeholder="Nouveau mot de passe">
+                <input class="form-control" data-valide type="password" name="password" id="password" placeholder="Nouveau mot de passe">
             </div>
 
             <div class="col">
                 <div class="form-group  d-flex align-items-center col-12 p-0">
-                    <label class="my-2 mr-0" for="dob">Date de naissance<input class="form-control col-9 mt-2" type="date" name="dob" id="dob"></label>
+                    <label class="my-2 mr-0" for="dob">Date de naissance<input data-valide class="form-control col-9 mt-2" type="date" name="dob" id="dob"></label>
                     <span class="note-date col-6">Pourquoi indiquer ma date de naissance ?</span>
                 </div>
 
                 <div class="form-group d-flex">
                     <div class="row">
                         <div class="gender">
-                            <input class="form-control" type="radio" name="gender" id="genderFemale" value="F"><label class="m-0 pl-2" for="genderFemale">Femme</label>
+                            <input data-valide class="form-control" type="radio" name="gender" id="genderFemale" value="F"><label class="m-0 pl-2" for="genderFemale">Femme</label>
                         </div>
                         <div class="gender">
-                            <input class="form-control" type="radio" name="gender" id="genderMale" value="M"><label class="m-0 pl-2" for="genderMale">Homme</label>
+                            <input data-valide class="form-control" type="radio" name="gender" id="genderMale" value="M"><label class="m-0 pl-2" for="genderMale">Homme</label>
                         </div>
                     </div>
                 </div>
@@ -54,11 +54,8 @@
                 <p class="cgu col-9 pl-0">En cliquant sur inscription, vous acceptez nos Conditions et indiquez que vous avez lu notre Politique d'utilisation des données, y compris notre Utilisation des cookies. Vous pourrez recevoir des notifications par texto de la part de Facebook et pouvez vous désabonner à tout moment</p>
             </div>
 
-
-
-
             <div class="form-group pl-4">
-                <input type="submit" value="Inscription" id="btn-register">
+                <button value="Inscription" id="btn-register">Inscription</button>
             </div>
 
     </form>
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,41 +1,102 @@
-console.log("Run JS")
+
 function validateEmail(email) {
     const re = /^[\w\.\-_]{1,}@[\w\.\-]{6,}/
     return re.test(email);
 }
 
-function emailIsValid(element, isValid) {
-    let elValue = element.value,
-        isNotEmpty = (elValue != '' || elValue != null)
 
-    if (isValid && isNotEmpty) {
-        if (element.classList.contains('invalid')) {
-            removeClass(element, 'invalid');
+function checkString(chaine) {
+    if (chaine.value != null && chaine.value.length > 3) {
+        chaine.dataset.valide = "true";
+        return true;
+    }
+    chaine.dataset.valide = "false";
+    return false;
+}
+
+function checkPassword(pass) {
+    if (pass.value != null && pass.value.length >= 6) {
+        pass.dataset.valide = "true";
+        return true;
+    }
+    pass.dataset.valide = "false";
+    return false;
+
+}
+
+
+function checkEmail(email) {
+    if (validateEmail(email.value)) {
+        email.dataset.valide = "true";
+        return true;
+    }
+    email.dataset.valide = "false";
+    return false;
+}
+
+function checkEmailConf(emailconf, email) {
+    if (validateEmail(emailconf.value)) {
+        if (emailconf.value === email.value) {
+            emailconf.dataset.valide = "true";
+            return true;
         } else {
-            addClass(element, 'valid');
+            emailconf.dataset.valide = "false";
+            return false//, "Les mots de passe ne sont pas identiques";
         }
+
     } else {
-        if (element.classList.contains('valid')) {
-            removeClass(element, 'valid');
-        } else {
-            addClass(element, 'invalid');
-        }
+        emailconf.dataset.valide = "false";
+        return false//, "Le format n'est pas valide";
     }
 }
 
-function addClass(element, elClass) {
-    element.classList.add(elClass);
+function checkDate(date) {
+    if (date.value != "") {
+        date.dataset.valide = "true";
+        return true;
+    }
+    date.dataset.valide = "false";
+    return false;
 }
 
-function removeClass(element, elClass) {
-    element.classList.remove(elClass);
+function checkGender(m, f) {
+    if (m.checked || f.checked) {
+        m.dataset.valide = "true";
+        f.dataset.valide = "true";
+        return true;
+    }
+    m.dataset.valide = "false";
+    f.dataset.valide = "false";
+    return false;
 }
 
-var emailInput = document.querySelector('.validateEmail');
+let eltForm = document.getElementById('form-register');
+let childsForm = eltForm.querySelectorAll('input');
+eltForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    console.log("submit");
 
-emailInput.addEventListener('keyup', function (event) {
-    let validatedEmail = validateEmail(this.value);
-    emailIsValid(this, validatedEmail);
-});
+    if (checkString(document.getElementById('lastname')) &&
+        checkString(document.getElementById('firstname')) &&
+        checkEmail(document.getElementById('email')) &&
+        checkEmailConf(document.getElementById('email-confirmation'), document.getElementById('email')) &&
+        checkPassword(document.getElementById('password')) &&
+        checkDate(document.getElementById('dob')) &&
+        checkGender(document.getElementById('genderMale'), document.getElementById('genderFemale'))
+    ) {
+        eltForm.submit();
+    }
+})
 
-emailIsValid(emailInput, validateEmail(emailInput.value));
+
+let email = document.getElementById('email');
+
+for (let i = 0; i < eltForm.length; i++) {
+    eltForm[i].addEventListener('focus', function () {
+
+
+        console.log("Je suis sur " + eltForm[i].getAttribute('name'))
+
+
+    });
+}
